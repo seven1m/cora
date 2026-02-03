fa969abe609c8df625ec2030323f3456392b3939
Tim Morgan <tim@timmorgan.org>
2026-02-02 20:19:22 -0600

    Show better failure messages for ruby specs

--- src/test/spec/spec_helper.rb
+++ src/test/spec/spec_helper.rb
@@ -11,10 +11,12 @@ def it(desc)
   begin
     yield
     $__passes = $__passes + 1
-  rescue
-    $__failures << [$__describe, desc]
+  rescue => e
+    $__failures << [$__describe, desc, e.message]
   end
 end
+
+class SpecFailedException < StandardError; end
 
 class SpecExpectation
   def initialize(actual)
@@ -22,8 +24,10 @@ class SpecExpectation
   end
 
   def ==(expected)
-    if @actual != expected
-      raise "mismatch"
+    if @actual == expected
+      :noop
+    else
+      raise SpecFailedException, "Expected: " + expected.inspect + "\nActual: " + @actual.inspect
     end
   end
 end
@@ -36,9 +40,12 @@ end
 
 def report_results
   if $__failures.length > 0
-    puts "FAILURES:"
-    puts $__failures.length.to_s + " failed"
-    puts "FAILED"
+    puts 'FAILURES (' + $__failures.length.to_s + '):'
+    puts
+    $__failures.each do |details|
+      puts details[0] + ': ' + details[1]
+      puts details[2]
+    end
   else
     puts "OK: " + $__passes.to_s + " passed"
   end
