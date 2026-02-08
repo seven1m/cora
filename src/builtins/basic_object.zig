d3f2a7e68845605d3c15f5b823a4f912836e44a9
Tim Morgan <tim@timmorgan.org>
2026-02-07 21:02:05 -0600

    Implement BasicObject#equal?

---
+++ src/builtins/basic_object.zig
@@ -0,0 +1,17 @@
+const vm_mod = @import("../vm.zig");
+const value = @import("../value.zig");
+
+const VM = vm_mod.VM;
+const VMError = vm_mod.VMError;
+const Block = vm_mod.Block;
+const Value = value.Value;
+
+pub fn register(vm: *VM) !void {
+    const equal_sym = try vm.intern("equal?");
+    try vm.basic_object_class.module.methods.put(equal_sym, .{ .builtin = &builtinBasicObjectEqual });
+}
+
+pub fn builtinBasicObjectEqual(vm: *VM, receiver: Value, args: []Value, _: ?Block) VMError!Value {
+    try vm.requireArgCount(args, 1);
+    return Value.boolean(receiver.objectId() == args[0].objectId());
+}
--- src/builtins/builtins.zig
+++ src/builtins/builtins.zig
@@ -3,6 +3,7 @@ const vm_mod = @import("../vm.zig");
 const VM = vm_mod.VM;
 
 const array = @import("array.zig");
+const basic_object = @import("basic_object.zig");
 const encoding = @import("encoding.zig");
 const exception = @import("exception.zig");
 const false_class = @import("false_class.zig");
@@ -18,6 +19,7 @@ const symbol = @import("symbol.zig");
 const true_class = @import("true_class.zig");
 
 pub fn registerAll(vm: *VM) !void {
+    try basic_object.register(vm);
     try kernel.register(vm);
     try object.register(vm);
     try module_builtin.register(vm);
