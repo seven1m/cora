34010dd34ffa7e509f685ca378f0d491bfba9f90
Tim Morgan <tim@timmorgan.org>
2026-02-27 21:11:33 -0600

    Include Comparable in String

--- src/vm.zig
+++ src/vm.zig
@@ -746,6 +746,7 @@ pub const VM = struct {
         try self.clearLastMatch();
 
         try self.includeModule(self.object_class, self.kernel_module);
+        try self.includeModule(self.string_class, comparable_module_val.toModuleObject());
 
         // Create top-level self (Ruby "main" object)
         self.main_self = try self.newInstance(self.object_class);
