01422082499da3e8d88e4d7be99879bc5c3cd093
Tim Morgan <tim@timmorgan.org>
2026-02-27 21:02:17 -0600

    Add Class.allocate

--- src/builtins/class.zig
+++ src/builtins/class.zig
@@ -9,9 +9,10 @@ const ClassObject = value.ClassObject;
 
 pub fn register(vm: *VM) !void {
     const class_new_sym = try vm.intern("new");
-    const class_class_val = Value.fromObject(vm.class_class);
-    const class_singleton = try vm.getOrCreateSingletonClass(class_class_val);
-    try class_singleton.module.methods.put(class_new_sym, .{ .method = .{ .builtin = &builtinClassNew } });
+    try vm.class_class.module.methods.put(class_new_sym, .{ .method = .{ .builtin = &builtinClassNew } });
+
+    const class_allocate_sym = try vm.intern("allocate");
+    try vm.class_class.module.methods.put(class_allocate_sym, .{ .method = .{ .builtin = &builtinClassAllocate } });
 
     const class_equal_sym = try vm.intern("==");
     try vm.class_class.module.methods.put(class_equal_sym, .{ .method = .{ .builtin = &builtinClassEqual } });
@@ -22,40 +23,55 @@ pub fn builtinClassNew(vm: *VM, receiver: Value, args: []Value, block: ?Block) V
         return vm.raiseExceptionFmt(vm.type_error_class, "receiver is not a Class", .{});
     }
 
-    try vm.requireArgCountRange(args, 0, 1);
+    const class_ptr = receiver.toClassObject();
 
-    var superclass: *ClassObject = vm.object_class;
-    if (args.len == 1) {
-        if (!args[0].isClass()) {
-            return vm.raiseExceptionFmt(vm.type_error_class, "superclass must be a Class", .{});
+    // Class.new([superclass]) creates anonymous classes.
+    if (class_ptr == vm.class_class) {
+        try vm.requireArgCountRange(args, 0, 1);
+
+        var superclass: *ClassObject = vm.object_class;
+        if (args.len == 1) {
+            if (!args[0].isClass()) {
+                return vm.raiseExceptionFmt(vm.type_error_class, "superclass must be a Class", .{});
+            }
+            superclass = args[0].toClassObject();
         }
-        superclass = args[0].toClassObject();
+
+        const anonymous_name = try vm.intern("<anonymous>");
+        const class_val = try vm.newClass(anonymous_name, superclass);
+
+        if (block) |blk| {
+            const yield_result = switch (blk.kind) {
+                .chunk => |chunk_blk| chunk_blk_result: {
+                    chunk_blk.chunk.lexical_scope = try vm.createLexicalScope(class_val, vm.current_lexical_scope);
+
+                    const class_body_block = Block{
+                        .kind = .{ .chunk = .{
+                            .chunk = chunk_blk.chunk,
+                            .defining_ep = chunk_blk.defining_ep,
+                            .defining_self = class_val,
+                        } },
+                    };
+                    break :chunk_blk_result try vm.yieldToBlock(class_body_block, &[_]Value{});
+                },
+                .symbol => try vm.yieldToBlock(blk, &[_]Value{}),
+                .builtin => try vm.yieldToBlock(blk, &[_]Value{}),
+            };
+            if (yield_result.break_occurred) return yield_result.value;
+        }
+
+        return class_val;
     }
 
-    const anonymous_name = try vm.intern("<anonymous>");
-    const class_val = try vm.newClass(anonymous_name, superclass);
-
-    if (block) |blk| {
-        const yield_result = switch (blk.kind) {
-            .chunk => |chunk_blk| chunk_blk_result: {
-                chunk_blk.chunk.lexical_scope = try vm.createLexicalScope(class_val, vm.current_lexical_scope);
-
-                const class_body_block = Block{
-                    .kind = .{ .chunk = .{
-                        .chunk = chunk_blk.chunk,
-                        .defining_ep = chunk_blk.defining_ep,
-                        .defining_self = class_val,
-                    } },
-                };
-                break :chunk_blk_result try vm.yieldToBlock(class_body_block, &[_]Value{});
-            },
-            .symbol => try vm.yieldToBlock(blk, &[_]Value{}),
-            .builtin => try vm.yieldToBlock(blk, &[_]Value{}),
-        };
-        if (yield_result.break_occurred) return yield_result.value;
+    // OtherClass.new(...) instantiates OtherClass and calls initialize.
+    const instance = try vm.newObjectForClass(class_ptr);
+    const init_sym = try vm.intern("initialize");
+    if (try vm.findMethod(instance, init_sym)) |_| {
+        _ = try vm.callMethodByName(instance, "initialize", args, block);
+    } else if (args.len != 0) {
+        try vm.requireArgCount(args, 0);
     }
-
-    return class_val;
+    return instance;
 }
 
 pub fn builtinClassEqual(vm: *VM, receiver: Value, args: []Value, _: ?Block) VMError!Value {
@@ -64,4 +80,18 @@ pub fn builtinClassEqual(vm: *VM, receiver: Value, args: []Value, _: ?Block) VME
         return Value.boolean(false);
     }
     return Value.boolean(receiver.toClassObject() == args[0].toClassObject());
+}
+
+pub fn builtinClassAllocate(vm: *VM, receiver: Value, args: []Value, _: ?Block) VMError!Value {
+    try vm.requireArgCount(args, 0);
+    if (!receiver.isClass()) {
+        return vm.raiseExceptionFmt(vm.type_error_class, "receiver is not a Class", .{});
+    }
+
+    const class_ptr = receiver.toClassObject();
+    if (class_ptr.object_type == .string) {
+        return vm.newStringForClassWithEncoding(class_ptr, "", false, .{ .ascii_8bit = .{} });
+    }
+
+    return vm.newObjectForClass(class_ptr);
 }
