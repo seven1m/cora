596961060c1e59b3cc972d9abca3e61e12fdc4bc
Tim Morgan <tim@timmorgan.org>
2026-05-20 22:19:13 -0500

    Set local GEM_HOME in harness

--- test/rubygems_harness.rb
+++ test/rubygems_harness.rb
@@ -2,6 +2,19 @@
 # Cora's stdlib lives on the default $LOAD_PATH, but vendored libraries remain
 # opt-in so harnesses control when they are visible.
 
+require "fileutils"
+
+repo_dir = File.expand_path("..", __dir__)
+gem_root = File.join(repo_dir, ".cora", "gems")
+spec_cache = File.join(repo_dir, ".cora", "gem-spec-cache")
+
+FileUtils.mkdir_p(gem_root)
+FileUtils.mkdir_p(spec_cache)
+
+ENV["GEM_HOME"] ||= gem_root
+ENV["GEM_PATH"] ||= gem_root
+ENV["GEM_SPEC_CACHE"] ||= spec_cache
+
 $LOAD_PATH.unshift(File.expand_path("../ext/rubygems/lib", __dir__))
 
 require "thread"
