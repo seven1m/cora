126c3e0ef26aa4ed52a842aba8776009c8dcdd6e
Tim Morgan <tim@timmorgan.org>
2026-05-31 14:44:08 -0500

    Add Pathname#expand_path, to_str, parent, join, exist?, basename

--- lib/stdlib/pathname.rb
+++ lib/stdlib/pathname.rb
@@ -9,7 +9,31 @@ class Pathname
 
   alias to_s to_path
 
+  def to_str
+    @path
+  end
+
   def dirname
     Pathname.new(File.dirname(@path))
+  end
+
+  def basename(*args)
+    Pathname.new(File.basename(@path, *args))
+  end
+
+  def expand_path(directory = nil)
+    Pathname.new(File.expand_path(@path, directory))
+  end
+
+  def parent
+    dirname
+  end
+
+  def join(*args)
+    Pathname.new(File.join(@path, *args))
+  end
+
+  def exist?
+    File.exist?(@path)
   end
 end
