571e14a80baf501b9e53777644cec66698e28607
Tim Morgan <tim@timmorgan.org>
2026-05-31 15:21:47 -0500

    Add more Pathname methods

--- lib/stdlib/pathname.rb
+++ lib/stdlib/pathname.rb
@@ -36,4 +36,96 @@ class Pathname
   def exist?
     File.exist?(@path)
   end
+
+  def file?
+    File.file?(@path)
+  end
+
+  def directory?
+    File.directory?(@path)
+  end
+
+  def symlink?
+    File.symlink?(@path)
+  end
+
+  def readable?
+    File.readable?(@path)
+  end
+
+  def writable?
+    File.writable?(@path)
+  end
+
+  def executable?
+    File.executable?(@path)
+  end
+
+  def size
+    File.size(@path)
+  end
+
+  def size?
+    File.size?(@path)
+  end
+
+  def zero?
+    File.zero?(@path)
+  end
+
+  def ftype
+    File.ftype(@path)
+  end
+
+  def stat
+    File.stat(@path)
+  end
+
+  def lstat
+    File.lstat(@path)
+  end
+
+  def children
+    Dir.children(@path).map { |child| Pathname.new(File.join(@path, child)) }
+  end
+
+  def each_child(&block)
+    children.each(&block)
+  end
+
+  def absolute?
+    @path.start_with?("/")
+  end
+
+  def relative?
+    !absolute?
+  end
+
+  def root?
+    @path == "/"
+  end
+
+  def relative_path_from(base)
+    base = base.to_s if base.is_a?(Pathname)
+    base = base.to_str
+
+    # Split paths into components
+    src_parts = @path.sub(/\/+\z/, "").split("/")
+    dst_parts = base.sub(/\/+\z/, "").split("/")
+
+    # Remove common prefix
+    common = 0
+    while common < src_parts.length && common < dst_parts.length && src_parts[common] == dst_parts[common]
+      common += 1
+    end
+
+    # Build result: "../" for each remaining component in base, then remaining in path
+    result = []
+    (dst_parts.length - common).times { result << ".." }
+    src_parts[common..].each { |part| result << part }
+
+    result = result.join("/")
+    result = "." if result.empty?
+    Pathname.new(result)
+  end
 end
