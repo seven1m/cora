344f585818ba196d7d701b03cad80679ec09f879
Tim Morgan <tim@timmorgan.org>
2026-09-12 18:03:29 -0500

    Add Enumerable#min

--- spec/TODO.md
+++ spec/TODO.md
@@ -465,7 +465,7 @@ Source baseline: ../ruby_spec compared to local spec/
  - [x] core/enumerable/max_spec.rb
 - [x] core/enumerable/member_spec.rb
 - [x] core/enumerable/min_by_spec.rb
-- [ ] core/enumerable/min_spec.rb
+- [x] core/enumerable/min_spec.rb
 - [x] core/enumerable/minmax_by_spec.rb
 - [x] core/enumerable/minmax_spec.rb
 - [x] core/enumerable/none_spec.rb
--- src/builtins/enumerable.zig
+++ src/builtins/enumerable.zig
@@ -110,6 +110,8 @@ pub fn register(vm: *VM) !void {
     try enumerable_val.toModuleObject().methods.put(tally_sym, value.MethodEntry.builtin(&builtinEnumerableTally, .{ .variadic = 0 }));
     const max_sym = try vm.intern("max");
     try enumerable_val.toModuleObject().methods.put(max_sym, value.MethodEntry.builtin(&builtinEnumerableMax, .{ .variadic = 0 }));
+    const min_sym = try vm.intern("min");
+    try enumerable_val.toModuleObject().methods.put(min_sym, value.MethodEntry.builtin(&builtinEnumerableMin, .{ .variadic = 0 }));
 }
 
 fn builtinEnumerableToSet(vm: *VM, receiver: Value, args: []Value, block: ?Block) VMError!Value {
@@ -1549,6 +1551,56 @@ fn builtinEnumerableMax(vm: *VM, receiver: Value, args: []Value, block: ?Block) 
         }
     }
     return max;
+}
+
+fn builtinEnumerableMin(vm: *VM, receiver: Value, args: []Value, block: ?Block) VMError!Value {
+    try vm.requireArgCountRange(args, 0, 1);
+
+    if (args.len == 1 and !args[0].isNil()) {
+        const n = try args[0].coerceToI64ViaToInt(
+            vm,
+            "no implicit conversion into Integer",
+            "no implicit conversion into Integer",
+            "bignum too big to convert into `long`",
+        );
+        if (n < 0) {
+            return vm.raiseExceptionFmt(vm.argument_error_class, "negative array size", .{});
+        }
+
+        const enum_value = try vm.createMethodEnumerator(receiver, try vm.intern("each"), &.{});
+        const all = try vm.createArray();
+        while (try enumerableNextElement(vm, enum_value)) |element| {
+            all.elements.append(vm.gc_allocator, element) catch return error.Fatal;
+        }
+        const all_value = Value.fromObject(&all.object);
+        const sorted = try vm.callMethodByName(all_value, "sort", &.{}, block);
+
+        const items = sorted.toArrayObject().elements.items;
+        const count: usize = @min(@as(usize, @intCast(n)), items.len);
+        const out = try vm.createArray();
+        var i: usize = 0;
+        while (i < count) : (i += 1) {
+            out.elements.append(vm.gc_allocator, items[i]) catch return error.Fatal;
+        }
+        return Value.fromObject(&out.object);
+    }
+
+    const enum_value = try vm.createMethodEnumerator(receiver, try vm.intern("each"), &.{});
+    const first = (try enumerableNextElement(vm, enum_value)) orelse return Value.nil();
+
+    var min = first;
+    if (block) |blk| {
+        while (try enumerableNextElement(vm, enum_value)) |item| {
+            const yield_args = [_]Value{ item, min };
+            const yielded = try vm.yieldToBlock(blk, &yield_args);
+            if ((try enumerableBlockResultSign(vm, yielded)) < 0) min = item;
+        }
+    } else {
+        while (try enumerableNextElement(vm, enum_value)) |item| {
+            if (try enumerableValueLessThan(vm, item, min)) min = item;
+        }
+    }
+    return min;
 }
 
 fn builtinEnumerableTally(vm: *VM, receiver: Value, args: []Value, _: ?Block) VMError!Value {
