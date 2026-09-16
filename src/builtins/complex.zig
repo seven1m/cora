00909a616373b215220b24a8c2955e5b219d9554
Tim Morgan <tim@timmorgan.org>
2026-09-15 20:07:19 -0500

    Add Math.asinh

--- src/builtins/complex.zig
+++ src/builtins/complex.zig
@@ -177,6 +177,9 @@ pub fn register(vm: *VM) !void {
         const acosh_sym = try vm.intern("acosh");
         try math_singleton.module.methods.put(acosh_sym, value.MethodEntry.builtin(&builtinMathAcosh, .{ .exact = 1 }));
         try math_entry.value.toModuleObject().methods.put(acosh_sym, value.MethodEntry.builtinWithVisibility(&builtinMathAcosh, .{ .exact = 1 }, .private));
+        const asinh_sym = try vm.intern("asinh");
+        try math_singleton.module.methods.put(asinh_sym, value.MethodEntry.builtin(&builtinMathAsinh, .{ .exact = 1 }));
+        try math_entry.value.toModuleObject().methods.put(asinh_sym, value.MethodEntry.builtinWithVisibility(&builtinMathAsinh, .{ .exact = 1 }, .private));
         const pi_sym = try vm.intern("PI");
         try math_entry.value.toModuleObject().constants.put(pi_sym, .{ .value = try vm.newFloat(std.math.pi) });
         const e_sym = try vm.intern("E");
@@ -831,6 +834,27 @@ fn builtinMathAcosh(vm: *VM, _: Value, args: []Value, _: ?Block) VMError!Value {
     if (f < 1.0)
         return vm.raiseExceptionFmt(vm.math_domain_error_class, "Numerical argument is out of domain - \"acosh\"", .{});
     return vm.newFloat(std.math.acosh(f));
+}
+
+fn builtinMathAsinh(vm: *VM, _: Value, args: []Value, _: ?Block) VMError!Value {
+    try vm.requireArgCount(args, 1);
+    const arg = args[0];
+    const f: f64 = if (arg.isFloat())
+        arg.toFloatObject().val
+    else if (arg.isInteger() or arg.isBigInteger())
+        arg.integerToF64()
+    else if (arg.isRational())
+        arg.toRationalObject().numerator.integerToF64() / arg.toRationalObject().denominator.integerToF64()
+    else if (vm.isClassOrSubclassOf(vm.getClass(arg), vm.numeric_class)) blk: {
+        // Mirrors MRI's rb_num_to_dbl: non-core Numerics convert via to_f.
+        const float_val = try vm.callMethodByName(arg, "to_f", &.{}, null);
+        if (!float_val.isFloat()) {
+            return vm.raiseExceptionFmt(vm.type_error_class, "can't convert {s} into Float", .{vm.className(arg)});
+        }
+        break :blk float_val.toFloatObject().val;
+    } else
+        return vm.raiseExceptionFmt(vm.type_error_class, "can't convert {s} into Float", .{vm.className(arg)});
+    return vm.newFloat(std.math.asinh(f));
 }
 
 fn builtinComplexDenominator(vm: *VM, receiver: Value, args: []Value, _: ?Block) VMError!Value {
