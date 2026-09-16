cb2db3150830565f65b1511906e3044ff6fbb4f3
Tim Morgan <tim@timmorgan.org>
2026-09-15 20:23:46 -0500

    Add Math.atanh

--- spec/TODO.md
+++ spec/TODO.md
@@ -1346,7 +1346,7 @@ Source baseline: ../ruby_spec compared to local spec/
 - [x] core/math/asinh_spec.rb
 - [x] core/math/atan2_spec.rb
 - [x] core/math/atan_spec.rb
-- [ ] core/math/atanh_spec.rb
+- [x] core/math/atanh_spec.rb
 - [x] core/math/cbrt_spec.rb
 - [ ] core/math/constants_spec.rb
 - [x] core/math/cos_spec.rb
--- src/builtins/complex.zig
+++ src/builtins/complex.zig
@@ -183,6 +183,9 @@ pub fn register(vm: *VM) !void {
         const atan2_sym = try vm.intern("atan2");
         try math_singleton.module.methods.put(atan2_sym, value.MethodEntry.builtin(&builtinMathAtan2, .{ .exact = 2 }));
         try math_entry.value.toModuleObject().methods.put(atan2_sym, value.MethodEntry.builtinWithVisibility(&builtinMathAtan2, .{ .exact = 2 }, .private));
+        const atanh_sym = try vm.intern("atanh");
+        try math_singleton.module.methods.put(atanh_sym, value.MethodEntry.builtin(&builtinMathAtanh, .{ .exact = 1 }));
+        try math_entry.value.toModuleObject().methods.put(atanh_sym, value.MethodEntry.builtinWithVisibility(&builtinMathAtanh, .{ .exact = 1 }, .private));
         const pi_sym = try vm.intern("PI");
         try math_entry.value.toModuleObject().constants.put(pi_sym, .{ .value = try vm.newFloat(std.math.pi) });
         const e_sym = try vm.intern("E");
@@ -881,6 +884,29 @@ fn builtinMathAtan2(vm: *VM, _: Value, args: []Value, _: ?Block) VMError!Value {
             return vm.raiseExceptionFmt(vm.type_error_class, "can't convert {s} into Float", .{vm.className(arg)});
     }
     return vm.newFloat(std.math.atan2(fs[0], fs[1]));
+}
+
+fn builtinMathAtanh(vm: *VM, _: Value, args: []Value, _: ?Block) VMError!Value {
+    try vm.requireArgCount(args, 1);
+    const arg = args[0];
+    const f: f64 = if (arg.isFloat())
+        arg.toFloatObject().val
+    else if (arg.isInteger() or arg.isBigInteger())
+        arg.integerToF64()
+    else if (arg.isRational())
+        arg.toRationalObject().numerator.integerToF64() / arg.toRationalObject().denominator.integerToF64()
+    else if (vm.isClassOrSubclassOf(vm.getClass(arg), vm.numeric_class)) blk: {
+        // Mirrors MRI's rb_num_to_dbl: non-core Numerics convert via to_f.
+        const float_val = try vm.callMethodByName(arg, "to_f", &.{}, null);
+        if (!float_val.isFloat()) {
+            return vm.raiseExceptionFmt(vm.type_error_class, "can't convert {s} into Float", .{vm.className(arg)});
+        }
+        break :blk float_val.toFloatObject().val;
+    } else
+        return vm.raiseExceptionFmt(vm.type_error_class, "can't convert {s} into Float", .{vm.className(arg)});
+    if (@abs(f) > 1.0)
+        return vm.raiseExceptionFmt(vm.math_domain_error_class, "Numerical argument is out of domain - \"atanh\"", .{});
+    return vm.newFloat(std.math.atanh(f));
 }
 
 fn builtinComplexDenominator(vm: *VM, receiver: Value, args: []Value, _: ?Block) VMError!Value {
