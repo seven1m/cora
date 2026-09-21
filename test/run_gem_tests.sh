3f98e69ed34d617a1c1ef9d63099b29a8dd8a9f6
Tim Morgan <tim@timmorgan.org>
2026-09-21 09:14:42 -0500

    Add Tilt gem tests

--- test/run_gem_tests.sh
+++ test/run_gem_tests.sh
@@ -6,6 +6,8 @@ repo_root=$(CDPATH= cd -- "$(dirname -- "$0")/.." && pwd)
 gem_root="$repo_root/test/gems"
 minitest_version="5.27.0"
 minitest_dir="$gem_root/minitest-$minitest_version"
+tilt_version="2.8.0"
+tilt_dir="$gem_root/tilt-$tilt_version"
 rack_version="3.2.7"
 rack_dir="$gem_root/rack-$rack_version"
 minitest_global_expectations_version="1.0.2"
@@ -26,6 +28,20 @@ fi
 cd "$minitest_dir"
 "$repo_root/build/bin/cora" --disable-gems -Ilib:test:. -e \
     'require "minitest/autorun"; (Dir["test/**/test_*.rb"] + Dir["test/**/*_test.rb"]).uniq.sort.each { |file| require file }'
+
+if [ ! -d "$tilt_dir/.git" ]; then
+    if [ -e "$tilt_dir" ]; then
+        echo "error: $tilt_dir exists but is not a git checkout" >&2
+        exit 1
+    fi
+
+    git clone --branch "v$tilt_version" --depth 1 \
+        https://github.com/jeremyevans/tilt.git "$tilt_dir"
+fi
+
+cd "$tilt_dir"
+"$repo_root/build/bin/cora" --disable-gems \
+    -I"$minitest_dir/lib:lib:test:." test/all.rb
 
 if [ ! -d "$minitest_global_expectations_dir/.git" ]; then
     if [ -e "$minitest_global_expectations_dir" ]; then
